c1c3a554a6464ab51bc485d2a9884d58ef075ac5
Christopher Stalls <christopherstalls@Christophers-MacBook-Pro.local>
2014-05-15 20:52:21 -0500

    finished product

--- Stalls_Christopher_assignment2.js
+++ Stalls_Christopher_assignment2.js
@@ -6,16 +6,19 @@
 
 //Starting Variables
 var carOn = false;
-var dayDriven = 0;
+var dayDriven = 1;
 var followRules = "Make sure to follow the road rules!";
 
 //prompt for who started the car
 var defaultDriver = prompt('Who started the car?', 'Chris');
 
+//call whosdriving function
+whosDriving(defaultDriver);
+
 //procedure for car start
 function whosDriving (defaultDriver)
 {
-	if (defaultDriver == 'Chris')
+	if (defaultDriver == 'Chris' && defaultDriver != 'null')
 		{
 			console.log('Chris is driving to work today.');
 		}
@@ -25,5 +28,55 @@ function whosDriving (defaultDriver)
 		}
 }
 
-//call whosdriving function
-whosDriving(defaultDriver);
+//confirm car start
+var carOn = confirm('Is the car started?');
+
+//Boolean function confirm car on and driver age
+function carStarted (carOn)
+{
+	do
+	{
+		var driverAge = parseInt(prompt('Input driver age.', '18'));
+		
+		if (driverAge >= 18)
+		{
+			console.log('Driver is allowed to drive.');
+			carOn == false;
+		}
+		else
+		{
+			console.log('Driver is not allowed to drive.');
+		}
+	}
+	while ((carOn == true) && (driverAge < 18))
+	return carOn;
+}
+
+//call carStarted function
+carStarted(carOn);
+
+function daysDriven (dayDriven)
+{
+	for(dayDriven = 1; dayDriven <= 7; dayDriven ++)
+	{
+		console.log('This is the amount of days driven: ' + dayDriven);
+	}
+	return dayDriven;
+}
+
+//call day driven
+daysDriven(dayDriven);
+
+function congratulations(followRules)
+{
+	console.log('Congratulations you can drive to work all week! ' + followRules);
+	return 0;
+}
+
+//call congratulations function
+congratulations(followRules);
+
+//return variables from functions
+console.log(carOn);
+console.log(dayDriven);
+console.log(congratulations());
